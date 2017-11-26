226cb6a5548e649f071b75f98f607bc302a09c21
Daniel Chong <danielgq.chong@gmail.com>
2017-11-27 08:14:35 +1300

    attempt to use captureScreen

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,3 +1,5 @@
+import { captureScreen } from 'react-native-view-shot'
+
 describe('Example', () => {
   beforeEach(async () => {
     await device.reloadReactNative()
@@ -5,5 +7,15 @@ describe('Example', () => {
 
   it('should have welcome screen', async () => {
     await expect(element(by.text('Welcome to React Native!'))).toBeVisible()
+  })
+
+  it('should have capture screen', async () => {
+    captureScreen({
+      format: 'jpg',
+      quality: 0.8,
+    }).then(
+      uri => console.log('Image saved to', uri),
+      error => console.error('Oops, snapshot failed', error),
+    )
   })
 })

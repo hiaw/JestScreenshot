f49cde28af8ffe156319e12023165785b9c82e0e
Daniel Chong <danielgq.chong@gmail.com>
2017-11-27 21:42:07 +1300

    able to match image snapshot

--- __tests__/App.js
+++ __tests__/App.js
@@ -1,12 +1,21 @@
-import 'react-native';
-import React from 'react';
-import App from '../App';
+import 'react-native'
+import React from 'react'
+import fs from 'fs'
+import { toMatchImageSnapshot } from 'jest-image-snapshot'
+
+import App from '../App'
+
+expect.extend({ toMatchImageSnapshot })
 
 // Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import renderer from 'react-test-renderer'
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <App />
-  );
-});
+  const tree = renderer.create(<App />)
+})
+
+it('should match capture screen', async () => {
+  fs.readFile('test.png', (err, data) => {
+    expect(data).toMatchImageSnapshot()
+  })
+})
